948f4799d861780aa67b78cce3142c6a0add9a7d
Szabolcs Kertesz <szabolcs_kertesz@epam.com>
2022-09-29 17:40:33 +0200

    Build simple modal component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
 import LargePersonListItem from 'ui/list-items/people/large-person-list-item'
 import SmallPersonListItem from 'ui/list-items/people/small-person-list-item'
 import LargeProductListItem from 'ui/list-items/products/large-product-list-item'
 import SmallProductListItem from 'ui/list-items/products/small-product-list-item'
 import NumberedList from 'ui/lists/numbered-list'
 import RegularList from 'ui/lists/regular-list'
+import Modal from 'ui/modals/modal'
 import './App.css'
 
 const people = [
@@ -57,11 +57,13 @@ function App() {
                 resourceName="person"
                 itemComponent={SmallPersonListItem}
             />
-            <NumberedList
-                items={people}
-                resourceName="person"
-                itemComponent={LargePersonListItem}
-            />
+            <Modal>
+                <NumberedList
+                    items={people}
+                    resourceName="person"
+                    itemComponent={LargePersonListItem}
+                />
+            </Modal>
             <RegularList
                 items={products}
                 resourceName="product"
---
+++ src/ui/modals/modal.tsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+import styled from 'styled-components'
+
+const ModalBackground = styled.div`
+    position: fixed;
+    z-index: 1;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    overflow: auto;
+    background-color: rgba(0, 0, 0, 0.5);
+`
+
+const ModalBody = styled.div`
+    background-color: white;
+    margin: 10% auto;
+    padding: 20px;
+    width: 50%;
+`
+
+function Modal({ children }: { children: React.ReactNode }) {
+    const [show, setShow] = useState(false)
+    return (
+        <>
+            <button onClick={() => setShow(true)}>Show modal</button>
+            {show && (
+                <ModalBackground onClick={() => setShow(false)}>
+                    <ModalBody onClick={e => e.stopPropagation()}>
+                        <button onClick={() => setShow(false)}>x</button>
+                        {children}
+                    </ModalBody>
+                </ModalBackground>
+            )}
+        </>
+    )
+}
+
+export default Modal
